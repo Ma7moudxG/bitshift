f662d045c561a9fa0b1b163d0b31986750c8159a
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-10-27 20:13:18 +0300

    fix navbar fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation"; // Updated hook
+import { usePathname } from "next/navigation";
 
 const navItems = {
   Home: "/",
@@ -15,114 +15,121 @@ const navItems = {
 };
 
 const Navbar = () => {
-  const pathname = usePathname(); // Get the current path
+  const pathname = usePathname();
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [header, setHeader] = useState(false);
 
   const scrollHeader = () => {
-    if (window.scrollY >= 20) setHeader(true);
+    if (window.scrollY >= 220) setHeader(true);
     else setHeader(false);
   };
 
   useEffect(() => {
     window.addEventListener("scroll", scrollHeader);
-    return () => {
-      window.addEventListener("scroll", scrollHeader);
-    };
+    return () => window.removeEventListener("scroll", scrollHeader);
   }, []);
 
   return (
-    <nav>
-      <div
-        className={
-          header ? "fixed z-10 bg-white w-[100%] p-3 shadow-md" : " p-4 sm:p-8 "
-        }
+    <>
+      <nav
+        className={`${
+          header ? "fixed top-0 left-0 w-full z-10 bg-white shadow-md" : ""
+        }`}
       >
-        <div className="flex items-center justify-between gap-4 max-w-[1200px] mx-auto">
-          <div className="flex">
+        <div
+          className={`${
+            header ? "p-3" : "p-4 sm:p-8"
+          } transition-all duration-300`}
+        >
+          <div className="flex items-center justify-between gap-4 max-w-[1200px] mx-auto">
             <Link href="/">
               <Image
                 src="/bitshift-logo.svg"
                 alt="bitshift logo"
                 height={160}
                 width={160}
+                className="w-[120px] md:w-[160px]"
               />
             </Link>
-          </div>
-          <ul className="items-center gap-8 lg:flex hidden">
-            {Object.entries(navItems).map(([label, url], index) => (
-              <li
-                key={index}
-                className={`text-[#02033B] hover:text-[#35BCDC] 
-                      ${pathname === url ? "font-bold text-[#35BCDC]" : ""}`}
-              >
-                <Link href={url}>{label}</Link>
-              </li>
-            ))}
-          </ul>
-          <section className="MOBILE-MENU flex lg:hidden">
-            <div
-              className="HAMBURGER-ICON space-y-2 cursor-pointer"
-              onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
-            >
-              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            </div>
-
-            <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-              <div
-                className="CROSS-ICON absolute top-0 right-0 px-8 py-8 cursor-pointer"
-                onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
-              >
-                <svg
-                  className="h-8 w-8 text-gray-600"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+            <ul className="items-center gap-8 lg:flex hidden">
+              {Object.entries(navItems).map(([label, url], index) => (
+                <li
+                  key={index}
+                  className={`text-[#02033B] hover:text-[#35BCDC] 
+                    ${pathname === url ? "font-bold text-[#35BCDC]" : ""}`}
                 >
-                  <line x1="18" y1="6" x2="6" y2="18" />
-                  <line x1="6" y1="6" x2="18" y2="18" />
-                </svg>
+                  <Link href={url}>{label}</Link>
+                </li>
+              ))}
+            </ul>
+            <section className="MOBILE-MENU flex lg:hidden">
+              <div
+                className="HAMBURGER-ICON space-y-2 cursor-pointer"
+                onClick={() => setIsNavOpen((prev) => !prev)}
+              >
+                <span className="block h-0.5 w-8 bg-gray-600"></span>
+                <span className="block h-0.5 w-8 bg-gray-600"></span>
+                <span className="block h-0.5 w-8 bg-gray-600"></span>
               </div>
-              <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] gap-12">
-                {Object.entries(navItems).map(([label, url], index) => (
-                  <li
-                    key={index}
-                    className="text-[#02033B] hover:text-[#35BCDC] text-xl"
-                    onClick={() => setIsNavOpen(false)}
+
+              <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                <div
+                  className="CROSS-ICON absolute top-0 right-0 px-8 py-8 cursor-pointer"
+                  onClick={() => setIsNavOpen(false)}
+                >
+                  <svg
+                    className="h-8 w-8 text-gray-600"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   >
-                    <Link href={url}>{label}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </section>
-          <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </div>
+                <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] gap-12">
+                  {Object.entries(navItems).map(([label, url], index) => (
+                    <li
+                      key={index}
+                      className="text-[#02033B] hover:text-[#35BCDC] text-xl"
+                      onClick={() => setIsNavOpen(false)}
+                    >
+                      <Link href={url}>{label}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </section>
+            <style>{`
+        .hideMenuNav {
+          display: none;
+        }
+        .showMenuNav {
+          display: block;
+          position: absolute;
+          width: 100%;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          background: white;
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+      `}</style>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      {/* Add a margin-top to main content only when the navbar is fixed */}
+      <main className={`${header ? "mt-[64px]" : ""}`}>
+        {/* Main content goes here */}
+      </main>
+    </>
   );
 };
 
