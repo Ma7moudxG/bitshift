7be0c6536a804cdfeab2d8cbf43822ff186e78bb
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-11-07 04:29:29 +0200

    update blogs and hide news

--- app/news-blogs/page.js
+++ app/news-blogs/page.js
@@ -23,71 +23,7 @@ export default function NewsBlogs() {
             </div>
         </div>
   
-        <div className='max-w-[1200px] mx-auto md:py-14 py-8'>
-          <div className="flex flex-col p-8 xl:p-0 md:py-14 py-8 gap-10 md:gap-14 max-w-[1200px] mx-auto items-center">
-            <h1 className="text-[#35BCDC] font-extrabold text-2xl md:text-3xl lg:text-4xl md:pr-8 text-centers">
-              NEWS
-            </h1>
-            
-            {/* Flex container for left and right sections */}
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                {/* Left section: First news item */}
-                <div className="flex flex-col gap-8 h-full">
-                    <Image 
-                        src={news[0].url}
-                        alt={news[0].title}
-                        height={800}
-                        width={600}
-                        layout="intrinsic"
-                        objectFit='cover'
-                    />
-                    <div className="flex flex-col gap-4">
-                        <p className="text-[#35BCDC] text-sm">{news[0].date}</p>
-                        <div className="flex justify-between">
-                            <h1 className="font-bold text-lg hover:text-[#35BCDC] cursor-pointer" onClick={() => router.push(`/news/${news[0].id}`)}>{news[0].title}</h1>
-                            <Image 
-                                src='/news/news-link.svg'
-                                alt="news link"
-                                width={24}
-                                height={24}
-                                onClick={() => router.push(`/news/${news[0].id}`)}
-                                className='cursor-pointer'
-                            />
-                        </div>
-                        <p className="text-[#667085] text-base">{news[0].description}</p>
-                    </div>
-                </div>
-
-                <hr className="block md:hidden my-8" />
-
-                {/* Right section: Grid with the next 6 news items */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 h-full">
-                    {news.slice(1, 7).map((blog, index) => (
-                        <div key={index} className="flex flex-col gap-2">
-                            {/* Fixed height for mobile, adjustable on larger screens */}
-                            <div className="relative w-full h-[180px] sm:h-[200px] lg:h-[140px]">
-                                <Image 
-                                src={blog.url}
-                                alt={blog.title}
-                                layout="fill"       
-                                objectFit="cover"  
-                                />
-                            </div>
-
-                            <div className="flex flex-col gap-2">
-                                <p className="text-[#35BCDC] text-xs">{blog.date}</p>
-
-                                {/* Truncate title after two lines */}
-                                <h1 className="font-bold text-base line-clamp-2 overflow-ellipsis hover:text-[#35BCDC] cursor-pointer" onClick={() => router.push(`/news/${blog.id}`)}>
-                                {blog.title}
-                                </h1>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                </div>
-            </div>
-        </div>
+        
 
         <div className="relative bg-[#F1F7FE] md:py-14 py-8">
           <div className="relative smax-w-[1200px] mx-auto">
@@ -131,3 +67,71 @@ export default function NewsBlogs() {
     </Main>
   );
 }
+
+
+{/* <div className='max-w-[1200px] mx-auto md:py-14 py-8'>
+          <div className="flex flex-col p-8 xl:p-0 md:py-14 py-8 gap-10 md:gap-14 max-w-[1200px] mx-auto items-center">
+            <h1 className="text-[#35BCDC] font-extrabold text-2xl md:text-3xl lg:text-4xl md:pr-8 text-centers">
+              NEWS
+            </h1>
+            
+            {/* Flex container for left and right sections */}
+            
+            // <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                {/* Left section: First news item */}
+                // <div className="flex flex-col gap-8 h-full">
+                //     <Image 
+                //         src={news[0].url}
+                //         alt={news[0].title}
+                //         height={800}
+                //         width={600}
+                //         layout="intrinsic"
+                //         objectFit='cover'
+                //     />
+                //     <div className="flex flex-col gap-4">
+                //         <p className="text-[#35BCDC] text-sm">{news[0].date}</p>
+                //         <div className="flex justify-between">
+                //             <h1 className="font-bold text-lg hover:text-[#35BCDC] cursor-pointer" onClick={() => router.push(`/news/${news[0].id}`)}>{news[0].title}</h1>
+                //             <Image 
+                //                 src='/news/news-link.svg'
+                //                 alt="news link"
+                //                 width={24}
+                //                 height={24}
+                //                 onClick={() => router.push(`/news/${news[0].id}`)}
+                //                 className='cursor-pointer'
+                //             />
+                //         </div>
+                //         <p className="text-[#667085] text-base">{news[0].description}</p>
+                //     </div>
+                // </div>
+
+                // <hr className="block md:hidden my-8" />
+
+                {/* Right section: Grid with the next 6 news items */}
+                // <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 h-full">
+                //     {news.slice(1, 7).map((blog, index) => (
+                //         <div key={index} className="flex flex-col gap-2">
+                //             {/* Fixed height for mobile, adjustable on larger screens */}
+                //             <div className="relative w-full h-[180px] sm:h-[200px] lg:h-[140px]">
+                //                 <Image 
+                //                 src={blog.url}
+                //                 alt={blog.title}
+                //                 layout="fill"       
+                //                 objectFit="cover"  
+                //                 />
+                //             </div>
+
+                //             <div className="flex flex-col gap-2">
+                //                 <p className="text-[#35BCDC] text-xs">{blog.date}</p>
+
+                                {/* Truncate title after two lines */}
+        //                         <h1 className="font-bold text-base line-clamp-2 overflow-ellipsis hover:text-[#35BCDC] cursor-pointer" onClick={() => router.push(`/news/${blog.id}`)}>
+        //                         {blog.title}
+        //                         </h1>
+        //                     </div>
+        //                 </div>
+        //             ))}
+        //         </div>
+        //         </div>
+        //     </div>
+        // </div> */}
--- components/NewsBlogs.js
+++ components/NewsBlogs.js
@@ -1,72 +1,84 @@
-'use client'
-import Image from 'next/image'
-import React from 'react'
-import { blogs } from './blogs'
-import { news } from './news'
-import { useRouter } from 'next/navigation'
+"use client";
+import Image from "next/image";
+import React from "react";
+import { blogs } from "./blogs";
+import { news } from "./news";
+import { useRouter } from "next/navigation";
 
 export default function NewsBlogs() {
-    const router = useRouter()
+  const router = useRouter();
   return (
-        <div className="flex flex-col p-8 sm:p-10 md:py-14 py-12 gap-10 md:gap-14 max-w-[1200px] mx-auto items-center">
-            <h1 className="text-[#35BCDC] font-extrabold text-2xl md:text-3xl lg:text-4xl md:pr-8 text-centers">
-                NEWS & BLOGS
-            </h1>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                <div className="flex flex-col gap-6">
-                    <Image 
-                        src={news[0].url}
-                        alt={news[0].title}
-                        height={432}
-                        width={592}
-                        layout="responsive"
-                    />
-                    <div className="flex flex-col gap-4">
-                        <p className="text-[#35BCDC] text-xs">{news[0].date}</p>
-                        <div className="flex justify-between">
-                            <h1 
-                                className="font-bold text-base hover:text-[#35BCDC] cursor-pointer" 
-                                onClick={() => router.push(`/news/${news[0].id}`)}>
-                                {news[0].title}
-                            </h1>
-                            <Image 
-                                src='/news/news-link.svg'
-                                alt="news link"
-                                width={24}
-                                height={24}
-                            />
-                        </div>
-                        <p className="text-[#667085] text-sm">{news[0].description}</p>
-                    </div>
-                </div>
-
-                <hr className="block md:hidden my-8" />
-                
-                <div className="flex flex-col gap-6">
-                    {blogs.slice(0, 2).map((blog, index) => (
-                        <div key={index} className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                            <div className="relative w-full h-[180px] sm:h-[220px] lg:h-[200px]">
-                                <Image 
-                                src={blog.url}
-                                alt={blog.title}
-                                layout="fill"       
-                                objectFit="cover"  
-                                />
-                            </div>
-                            <div className="flex flex-col gap-4">
-                                <p className="text-[#35BCDC] text-xs">{blog.date}</p>
-                                <h1 
-                                    className="font-bold text-base hover:text-[#35BCDC] cursor-pointer" 
-                                    onClick={() => router.push(`/blogs/${blog.id}`)}> 
-                                    {blog.title}
-                                </h1>
-
-                                <div className='line-clamp-4 text-[#667085] text-sm' dangerouslySetInnerHTML={{ __html: blog.description }} />
-                            </div>
-                        </div>
-                    ))}
-                </div>
+    <div className="flex flex-col p-8 sm:p-10 md:py-14 py-12 gap-10 md:gap-14 max-w-[1200px] mx-auto items-center">
+      <h1 className="text-[#35BCDC] font-extrabold text-2xl md:text-3xl lg:text-4xl md:pr-8 text-centers">
+        NEWS & BLOGS
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="flex flex-col gap-4">
+          <div className="relative w-full h-[180px] md:h-[300px]">
+            <Image
+              src={blogs[0].url}
+              alt={blogs[0].title}
+              layout="fill"
+              objectFit="cover"
+              className="cursor-pointer"
+              onClick={() => router.push(`/blogs/${blogs[0].id}`)}
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="text-[#35BCDC] text-xs">{blogs[0].date}</p>
+            <div className="flex justify-between">
+              <h1
+                className="font-bold text-base hover:text-[#35BCDC] cursor-pointer"
+                onClick={() => router.push(`/blogs/${blogs[0].id}`)}
+              >
+                {blogs[0].title}
+              </h1>
+              <Image
+                src="/news/news-link.svg"
+                alt="news link"
+                width={24}
+                height={24}
+              />
             </div>
+            <p className="line-clamp-1 text-[#667085] text-sm">
+              {blogs[0].description}
+            </p>
+          </div>
         </div>
-  )
+
+        <hr className="block md:hidden my-8" />
+
+        <div className="flex flex-col gap-6">
+          {blogs.slice(1, 3).map((blog, index) => (
+            <div key={index} className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="relative w-full h-[180px] sm:h-[220px] lg:h-[200px]">
+                <Image
+                  src={blog.url}
+                  alt={blog.title}
+                  layout="fill"
+                  objectFit="cover"
+                  className="cursor-pointer"
+                  onClick={() => router.push(`/blogs/${blog.id}`)}
+                />
+              </div>
+              <div className="flex flex-col gap-4">
+                <p className="text-[#35BCDC] text-xs">{blog.date}</p>
+                <h1
+                  className="font-bold text-base hover:text-[#35BCDC] cursor-pointer"
+                  onClick={() => router.push(`/blogs/${blog.id}`)}
+                >
+                  {blog.title}
+                </h1>
+
+                <div
+                  className="line-clamp-4 text-[#667085] text-sm"
+                  dangerouslySetInnerHTML={{ __html: blog.description }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
